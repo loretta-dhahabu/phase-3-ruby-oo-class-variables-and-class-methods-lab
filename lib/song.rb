ca2281f084f3a5a283885da2ba2c9a41e087b3d4
loretta-dhahabu <loreydjefwa@gmail.com>
2022-08-23 21:56:20 +0300

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,34 @@
+class Song
+    attr_accessor :name, :artist, :genre
+
+    @@count=0
+    @@artists=[]
+    @@genres=[]
+    @@artist_count={}
+    @@genre_count={}
+
+    def initialize(name, artist, genre)
+        @name=name
+        @artist=artist
+        @genre=genre
+        @@count += 1
+        @@artists<<artist
+        @@genres<<genre
+    end
+    def self.count
+        @@count
+    end
+    def self.artists
+        @@artists.uniq
+    end
+    def self.genres
+        @@genres.uniq
+    end
+    
+    def self.artist_count
+        @@artist_count = @@artists.tally
+    end
+    def self.genre_count
+        @@genre_count = @@genres.tally
+    end
+end
